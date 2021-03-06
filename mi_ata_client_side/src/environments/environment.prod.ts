13db9109a80e53c90d81425518e988c538f2cda0
Batel Katani <batelkat@post.bgu.ac.il>
2021-03-06 18:13:34 +0200

    init commit

---
+++ mi_ata_client_side/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  baseApi: 'https://limitless-garden-71922.herokuapp.com/api',
+  serverAddress: 'https://limitless-garden-71922.herokuapp.com'
+};
